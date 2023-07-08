5eafa3ead7fbee22078643b274999c53d2b1177b
João Henrique de Oliveira <68037457+oliveirajh@users.noreply.github.com>
2023-07-08 10:43:01 -0300

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
 import Image from 'next/image'
 import profile from './../../public/img/profile.jpg';
-import '../css/App.css';
+
+import styles from "./../css/Main.module.css"
+import "../css/global.css"
 
 export default function Home() {
   return (
-      <header className="App-header">
-        <Image src={profile} alt="Profile" className="profile"/>
-        <p className="Title">
-          OLIVEIRA JH PORTFOLIO
-        </p>
-        <p className="App-link">
-          Coming Soon.
-        </p>
-      </header>
+      <>
+        <header className={styles.header}>
+          <div className={styles.head}>
+            <p className={styles.logo}>OLIVEIRA JH</p>
+          </div>
+        </header>
+        <div className={styles.Appheader}>
+          <Image src={profile} alt="Profile" className={styles.profile} />
+          <p className={styles.Title}>
+            OLIVEIRA JH PORTFOLIO
+          </p>
+          <p className={styles.Applink}>
+            Coming Soon.
+          </p>
+        </div>
+      </>
   )
 }
